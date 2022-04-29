87d81c9fb91e0fd7a8dce0140e95019468d60719
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-30 03:41:37 +0600

    signup with email and password authentication added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,13 +1,46 @@
+import React, { useEffect } from 'react';
 import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useUpdateProfile } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 import './Register.css';
 
 const Register = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = (data) => console.log(data);
+    const navigate = useNavigate();
+
+    // Auth for create account with email and password
+    const [createUserWithEmailAndPassword, user, loading,
+        error,] =
+        useCreateUserWithEmailAndPassword(auth);
+
+    // auth for updating name
+    const [updateProfile] = useUpdateProfile(auth);
+
+    // crate user with email and pass
+    const onSubmit = async ({ name, email, password, confirmPass }) => {
+        const displayName = name;
+
+        console.log(email, password)
+
+        if (password !== confirmPass) {
+            return console.log('password not matched');
+        }
+
+        await createUserWithEmailAndPassword(email, password);
+        await updateProfile({ displayName });
+    };
+
+    useEffect(() => {
+        if(user) {
+            navigate('/')
+        }
+    }, [user])
+
+    console.log(user)
 
     return (
         <div className="form-container">
@@ -62,8 +95,7 @@ const Register = () => {
                 </form>
                 <div className="user-help">
                     <p>
-                        Already have an account?{' '}
-                        <Link to="/login">Login</Link>
+                        Already have an account? <Link to="/login">Login</Link>
                     </p>
                 </div>
                 <div className="divider">OR</div>
